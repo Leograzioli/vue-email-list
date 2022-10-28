7cb59aa6d776df1cc6ddcdd683b0063e7c924878
Leograzioli <leograzioli@gmail.com>
2022-10-28 14:59:42 +0200

    sistemato css e html

--- js/script.js
+++ js/script.js
@@ -3,7 +3,17 @@ const {createApp} = Vue;
 createApp({
     data() {
         return {
-            
+
         }
+    },
+    methods: {
+
+    },
+    created() {
+        axios
+        .get(`https://flynn.boolean.careers/exercises/api/random/mail`)
+        .then((resp) => {
+            console.log(resp.data.response)
+        })
     }
 }).mount("#app")
