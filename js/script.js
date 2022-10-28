c7238b966931b5c0991e617fd685c5611e8a19af
Leograzioli <leograzioli@gmail.com>
2022-10-28 15:19:09 +0200

    add loop for nel axios e stampato 10 email nel html

--- js/script.js
+++ js/script.js
@@ -3,17 +3,21 @@ const {createApp} = Vue;
 createApp({
     data() {
         return {
-
+            emailArray: []
         }
     },
     methods: {
 
     },
     created() {
-        axios
-        .get(`https://flynn.boolean.careers/exercises/api/random/mail`)
-        .then((resp) => {
-            console.log(resp.data.response)
-        })
+        for (let i = 0; i < 10; i++) {
+            
+            axios
+            .get(`https://flynn.boolean.careers/exercises/api/random/mail`)
+            .then((resp) => {
+                this.emailArray.push(resp.data.response)
+            })
+            
+        }
     }
 }).mount("#app")
